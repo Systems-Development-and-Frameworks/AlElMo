c257ef1ea929cf0d0e20fe913ede08347aa80bce
Momo <hohenheim@mailbox.org>
2020-10-21 02:37:49 +0200

    First Try List

---
+++ App/src/components/ItemList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div v-for="item in itemsArr" :key="item.title">
+      <Item
+        :title="item.title"
+        :votes="item.votes"
+        v-on:changeVotes="updateVotes($event)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Item from "./Item.vue";
+
+export default {
+  data() {
+    return {
+      itemsArr: [
+        { title: "Al", votes: 3 },
+        { title: "El", votes: 5 },
+        { title: "Mo", votes: 1 },
+      ],
+    };
+  },
+  props: {},
+  methods: {
+    updateVotes: function (item) {
+      this.itemsArr.find(item.title).votes = item.votes;
+    },
+  },
+  computed: {},
+  components: {
+    Item,
+  },
+  mounted() {},
+};
+</script>
+
+<style></style>
