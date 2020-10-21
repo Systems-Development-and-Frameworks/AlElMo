2b76272e0657b62529a55e185252ce0c9febf20f
Momo <hohenheim@mailbox.org>
2020-10-21 02:37:49 +0200

    First Try List

---
+++ App/src/components/Item.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <h1>{{ title }} ({{ votes }})</h1>
+    </div>
+    <div>
+      <b-button v-on:click="changeVotes(votes++)"> Upvote</b-button>
+      <b-button v-on:click="changeVotes(votes--)"> Downvote</b-button>
+      <b-button variant="Remove"> Remove</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    votes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  methods: {
+    changeVotes: function () {
+      this.$emit("changeVotes", this);
+    },
+  },
+  computed: {},
+  components: {},
+  mounted() {},
+};
+</script>
+
+<style></style>
