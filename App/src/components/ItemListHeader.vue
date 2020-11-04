7731415d2e68a461d784110eba5b6553b12125ee
JaielSoft <Elias.Kechter@t-online.de>
2020-11-04 11:10:27 +0100

    changed code for consistency and applied PR change suggestions

---
+++ App/src/components/ItemListHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-row>
+    <b-col cols="12" class="font-weight-bold">
+      <h1>Item List</h1>
+    </b-col>
+    <b-col>
+      <b-button class="mb-5" @click="click">
+        Ordering: {{ desc ? "Desc" : "Asc" }}
+      </b-button>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  methods: {
+    click() {
+      this.$emit("click");
+    },
+  },
+  props: {
+    desc: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
