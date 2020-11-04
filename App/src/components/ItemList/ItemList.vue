6bf5b908f53505b6bd6f3f2ec9c58b02a7487945
JaielSoft <Elias.Kechter@t-online.de>
2020-11-04 17:24:52 +0100

    resolved merge fail

--- App/src/components/ItemList/ItemList.vue
+++ App/src/components/ItemList/ItemList.vue
@@ -1,26 +1,25 @@
 <template>
-  <div>
-    <template v-if="itemsArrOrdered.length">
-      <div
+  <b-container>
+    <item-list-header :desc="desc" @click="toggleOrdering" />
+    <template v-if="itemsArr.length">
+      <item
         v-for="item in itemsArrOrdered"
         :key="item.id"
-        style="margin-bottom: 1em"
-      >
-        <Item
-          :item="item"
-          v-on:changeVotes="updateVotes($event)"
-          v-on:remove="removeItem($event)"
-        />
-      </div>
+        :item="item"
+        @changeVotes="updateVotes"
+        @remove="removeItem"
+      />
     </template>
-    <div v-else id="itemlist-emptyxxx">Itemlist empty :(</div>
-    <AddItemForm v-on:addItem="addItem($event)" />
-  </div>
+    <empty-item-list v-else />
+    <add-item-form @addItem="addItem" />
+  </b-container>
 </template>
 
 <script>
 import Item from "../Item.vue";
 import AddItemForm from "../AddItemForm.vue";
+import EmptyItemList from "../EmptyItemList.vue";
+import ItemListHeader from "../ItemListHeader.vue";
 
 export default {
   data() {
@@ -30,33 +29,53 @@ export default {
         { id: 2, title: "El", votes: 5 },
         { id: 3, title: "Mo", votes: 1 },
       ],
-      id: 4,
+      desc: true,
     };
   },
-  props: {},
   methods: {
-    updateVotes: function (item) {
+    updateVotes(item) {
       this.itemsArr = this.itemsArr.map((e) => (item.id === e.id ? item : e));
     },
-    removeItem: function (item) {
+    removeItem(item) {
       this.itemsArr = this.itemsArr.filter((e) => e.id != item.id);
     },
-    addItem: function (title) {
-      this.itemsArr.push({ id: this.id, title: title, votes: 0 });
-      this.id++;
+    addItem(item) {
+      const { title } = item;
+      this.itemsArr.push({ id: this.createId(), title, votes: 0 });
+    },
+    createId(length = 15) {
+      let text = `item_${Date.now()}_`;
+      const possible =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?=)(/&%$§\"!,.-;:_+*#'";
+      for (let i = 0; i < length; i += 1) {
+        text += possible.charAt(Math.random() * possible.length);
+      }
+      return text;
+    },
+    cloneArray(arr) {
+      return (Array.isArray(arr) && arr.slice()) || [];
+    },
+    sortItems(arr) {
+      const multiplier = this.desc ? 1 : -1;
+      return this.cloneArray(arr).sort(
+        (e1, e2) => multiplier * (e2.votes - e1.votes)
+      );
+    },
+    toggleOrdering() {
+      this.desc = !this.desc;
     },
   },
   computed: {
     itemsArrOrdered() {
-      return this.itemsArr.slice().sort((e1, e2) => e2.votes - e1.votes);
+      return this.sortItems(this.itemsArr);
     },
   },
   components: {
     Item,
     AddItemForm,
+    EmptyItemList,
+    ItemListHeader,
   },
   mounted() {},
 };
 </script>
-
-<style></style>
