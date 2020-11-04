79882d4689b54345fb649a4977f0990095c098a6
JaielSoft <Elias.Kechter@t-online.de>
2020-11-04 14:13:56 +0100

    implemented tests

--- App/jest.config.js
+++ App/jest.config.js
@@ -2,11 +2,9 @@ module.exports = {
     "moduleFileExtensions": [
         "js",
         "json",
-        // tell Jest to handle `*.vue` files
         "vue"
     ],
     "transform": {
-        // process `*.vue` files with `vue-jest`
         ".*\\.(js)$": "babel-jest",
         ".*\\.(vue)$": "vue-jest"
     },
