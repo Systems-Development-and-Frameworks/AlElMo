e55d37ba695badf8c721f039c8f3410da8741d50
JaielSoft <Elias.Kechter@t-online.de>
2020-11-05 23:37:12 +0100

    eslint configured

--- App/src/components/AddItemForm.vue
+++ App/src/components/AddItemForm.vue
@@ -1,10 +1,24 @@
 <template>
-  <b-row align-h="center" class="mt-3">
-    <b-col cols="5" class="pr-1">
-      <b-form-input debounce="300" v-model.trim="title" />
+  <b-row
+    align-h="center"
+    class="mt-3"
+  >
+    <b-col
+      cols="5"
+      class="pr-1"
+    >
+      <b-form-input
+        v-model.trim="title"
+        debounce="300"
+      />
     </b-col>
-    <b-col cols="3" class="pl-1 text-left">
-      <b-button @click="addItem">Add new item</b-button>
+    <b-col
+      cols="3"
+      class="pl-1 text-left"
+    >
+      <b-button @click="addItem">
+        Add new item
+      </b-button>
     </b-col>
   </b-row>
 </template>
