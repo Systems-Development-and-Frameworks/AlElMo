47f0d386d0d49c7d585ff205fab19a8b2db1e2ed
AlexHTW <s0544642@htw-berlin.de>
2020-11-09 11:47:38 +0100

    added stories for itemlistheader

--- App/src/components/Item/Item.stories.js
+++ App/src/components/Item/Item.stories.js
@@ -1,17 +1,18 @@
 import Item from './Item'
 
 export default {
-    title: 'Item',
-    compontent: Item,
+  title: 'Item',
+  compontent: Item,
 };
 
 const Template = (args, { argTypes }) => ({
   components: { Item },
-  template: '<Item v-bind="$props" />',
+  props: Object.keys(argTypes),
+  template: '<Item :item="item"/>',
 })
 
 export const Primary = Template.bind({});
 
 Primary.args = {
-  item: {id: 1, title: 'test', votes: 0},
+  item: { id: 1, title: 'test', votes: 0 },
 }
