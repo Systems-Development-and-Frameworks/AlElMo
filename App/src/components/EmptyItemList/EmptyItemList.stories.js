554289543a056e070bdaac9f78b405eb5e5e5919
JaielSoft <Elias.Kechter@t-online.de>
2020-11-11 00:00:26 +0100

    fixed some issues

--- App/src/components/EmptyItemList/EmptyItemList.stories.js
+++ App/src/components/EmptyItemList/EmptyItemList.stories.js
@@ -4,19 +4,17 @@ export default {
     title: 'EmptyItemList',
     compontent: EmptyItemList,
     argTypes: {
-        /*
-        todo - fix message: text not rendering from story or control
         message: { control: 'text' }
-        */
     },
 };
 
 const Template = (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
     components: { EmptyItemList },
     template: '<EmptyItemList v-bind="$props"/>',
 });
 
 export const EmptyList = Template.bind({});
 EmptyList.args = {
-    // message: "empty list message",
+    message: "List is empty :(",
 };
