23649f7408843cbf4774505c57d6cd8cefdcd198
Momo <hohenheim@mailbox.org>
2020-11-11 19:03:55 +0100

    Resolver und Typdef abgestimmt auf Posts und Users

--- backend/src/resolver.js
+++ backend/src/resolver.js
@@ -1,19 +1,54 @@
 
-const books = [
+
+const User1 = {
+    name: "3asdasd",
+    posts: [2]
+};
+
+
+
+var User2 = {
+    name: "4asdasdasd",
+    posts: [21]
+};
+
+
+const Post1 = {
+    id: 2,
+    title: "Hallo",
+    votes: 2,
+    author: "3asdasd"
+};
+
+const Posts = [
+    Post1,
     {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
+        id: 21,
+        title: "Hallo1",
+        votes: 21,
+        author: "4asdasdasd"
     },
 ];
 
 
+const Users = [User1, User2];
+
+
 module.exports = {
     Query: {
-        books: () => books,
+        posts: () => Posts,
+        users: () => Users
     },
+    Post: {
+        author(parent) {
+            return Users.find(user => user.name === parent.author);
+
+        }
+    },
+    User: {
+        posts(parent) {
+            return Posts.filter(post => parent.posts.includes(post.id));
+        }
+    }
 };
 
