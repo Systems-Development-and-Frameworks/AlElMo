2b5aafc2455965a6eef7c4917d6fee983446c10e
Momo <hohenheim@mailbox.org>
2020-11-11 19:03:55 +0100

    Resolver und Typdef abgestimmt auf Posts und Users

--- backend/src/typeDefs.js
+++ backend/src/typeDefs.js
@@ -5,16 +5,25 @@ module.exports = gql`
 # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
 # This "Book" type defines the queryable fields for every book in our data source.
-type Book {
-  title: String
-  author: String
+type Post {
+  id: ID!
+  title: String!
+  votes: Int!
+  author: User!
 }
+
+type User {
+  name: ID!
+  posts: [Post]
+}
+
 
 # The "Query" type is special: it lists all of the available queries that
 # clients can execute, along with the return type for each. In this
 # case, the "books" query returns an array of zero or more Books (defined above).
 type Query {
-  books: [Book]
+  posts: [Post],
+  users: [User]
 }
 `;
 
