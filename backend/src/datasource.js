b42558fbafcda7665b6da9b07e1158a067b702d2
Momo <hohenheim@mailbox.org>
2020-11-18 18:37:20 +0100

    Add Database, yml, mutations

---
+++ backend/src/datasource.js
@@ -0,0 +1,39 @@
+var User1 = {
+    name: "Al",
+    posts: []
+};
+var User2 = {
+    name: "El",
+    posts: []
+};
+var User3 = {
+    name: "Mo",
+    posts: []
+};
+
+const Post1 = {
+    id: 1,
+    title: "ABC",
+    votes: 2,
+    author: "Al",
+    usersUpvoted: []
+};
+const Post2 = {
+    id: 2,
+    title: "DEF",
+    votes: 1,
+    author: "El",
+    usersUpvoted: []
+};
+const Post3 = {
+    id: 3,
+    title: "GHI",
+    votes: 0,
+    author: "Mo",
+    usersUpvoted: []
+};
+
+
+
+exports.Users = [User1, User2, User3]
+exports.Posts = [Post1, Post2, Post3]
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,33 @@
+//init DB
+import { Post, InMemoryDataSource } from './db'
+const { Posts, Users } = require("./datasource")
+const db = new InMemoryDataSource(Posts, Users)
+const dataSources = () => ({ db })
+
+//Server
 const { ApolloServer, gql } = require('apollo-server');
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
+const context = ({ req, res }) => ({ req, res })
+
 const resolvers = require('./resolver.js');
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = require('./typeDefs.js');
 
+const opts = {
+  settings: {
+    'schema.polling.enable': false
+  }
+}
 
-
-
-
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources,
+  context,
+  opts
+});
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
-});
+}); 
