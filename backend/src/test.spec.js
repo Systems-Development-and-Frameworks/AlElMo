bd5ad872533b289193b283152ed6f5216e2e58ba
JaielSoft <Elias.Kechter@t-online.de>
2020-11-25 05:20:19 +0100

    refactored to use import/export, added delete a post and test, reworked the classes and isolated into files many many more things

--- backend/src/test.spec.js
+++ backend/src/test.spec.js
@@ -1,50 +1,53 @@
-const { createTestClient } = require('apollo-server-testing');
+import { createTestClient } from 'apollo-server-testing';
+import { ApolloServer } from 'apollo-server';
+import InMemoryDataSource from './main/InMemoryDataSource';
+import Post from './main/Post';
+import User from './main/User';
 
-const { ApolloServer } = require('apollo-server');
-const { InMemoryDataSource } = require('./db');
-
-const resolvers = require('./resolver.js');
-const typeDefs = require('./typeDefs.js');
+import resolvers from './resolver';
+import typeDefs from './typeDefs';
 
 const context = ({ req, res }) => ({ req, res });
 
-const getPost = (id = '1', title = 'ABC', votes = 0, author = 'Al', usersUpvoted = [], usersDownvoted = []) => {
-  return {
-    id,
-    title,
-    votes,
-    author,
-    usersUpvoted,
-    usersDownvoted
+const getPost = (id = '1', title = 'ABC', author = 'Al', upvotes = [], downvotes = []) => {
+  const data = {
+    post: {
+      title,
+      author: {
+        name: author,
+      },
+    },
   };
+  return new Post(data, id)
+    .setId(id)
+    .setTitle(title)
+    .setAuthor(author)
+    .setUpvotes(upvotes)
+    .setDownvotes(downvotes);
 };
 
-const getUser = (name = 'Al', posts = ['1']) => {
-  return {
-    name, posts
-  };
-};
+const getUser = (name = 'Al', posts = ['1']) => new User({ name })
+  .setName(name)
+  .setPosts(posts);
 
-const getQuery = (Posts, Users) => {
-  const db = new InMemoryDataSource(Posts, Users);
+const setupServer = (users, posts) => {
+  const db = new InMemoryDataSource(users, posts);
   const dataSources = () => ({ db });
 
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     dataSources,
-    context
+    context,
   });
-  return createTestClient(server).query;
+  return { ...createTestClient(server), db };
 };
 
 describe('Testing Apollo Server', () => {
-  it('Query: Receive one Post, when asking for Posts', async () => {
-    // Setup Server
-
-    const Posts = [getPost()];
-    const Users = [getUser()];
-    const query = getQuery(Posts, Users);
+  it('Query: Receive one post, when asking for posts', async () => {
+    const posts = [getPost()];
+    const users = [getUser()];
+    const { query } = setupServer(users, posts);
 
     const GET_POSTS = '{ posts { id, votes, title }}';
 
@@ -56,12 +59,11 @@ describe('Testing Apollo Server', () => {
     expect(response.data.posts[0].title).toEqual('ABC');
     expect(response.data.posts[0].votes).toEqual(0);
   });
-  it('Query: Receive one User, when asking for Users', async () => {
-    // Setup Server
 
-    const Posts = [getPost()];
-    const Users = [getUser()];
-    const query = getQuery(Posts, Users);
+  it('Query: Receive one user, when asking for users', async () => {
+    const posts = [getPost()];
+    const users = [getUser()];
+    const { query } = setupServer(users, posts);
 
     const GET_POSTS = '{ users { name }}';
 
@@ -71,12 +73,11 @@ describe('Testing Apollo Server', () => {
     expect(response.data.users.length).toEqual(1);
     expect(response.data.users[0].name).toEqual('Al');
   });
-  it('Query: Receive one User, their posts, the author of one of their post and the name of this author, when asking for Users', async () => {
-    // Setup Server
 
-    const Posts = [getPost()];
-    const Users = [getUser()];
-    const query = getQuery(Posts, Users);
+  it('Query: Receive one user, their posts, the author of one of their post and the name of this author, when asking for users', async () => {
+    const posts = [getPost()];
+    const users = [getUser()];
+    const { query } = setupServer(users, posts);
 
     const GET_POSTS = '{ users { name, posts {title, author{name}} }}';
 
@@ -88,122 +89,135 @@ describe('Testing Apollo Server', () => {
     expect(response.data.users[0].posts[0].title).toEqual('ABC');
     expect(response.data.users[0].posts[0].author.name).toEqual('Al');
   });
+
   it('Mutation: Create a post', async () => {
-    // Setup Server
+    const posts = [];
+    const users = [getUser()];
+    const { mutate } = setupServer(users, posts);
 
-    const Posts = [];
-    const Users = [getUser()];
-    const query = getQuery(Posts, Users);
-
-    const Post = getPost();
-    const CREATE_POST = `mutation PostInput{ write(post: {title:"${Post.title}", author:{name:"${Post.author}"}})
+    const post = getPost();
+    const CREATE_POST = `mutation { write(post: {title:"${post.title}", author:{name:"${post.author}"}})
     { title, votes, author{name, posts{id}}, id}}`;
-    const response = await query({ query: CREATE_POST });
+    const response = await mutate({ mutation: CREATE_POST });
     expect(response.errors).toBeUndefined();
     expect(response.data.write.title).toEqual('ABC');
     expect(response.data.write.author.name).toEqual('Al');
-    // User should have the Post in his/her lists of posts
-    console.log(response.data);
     expect(response.data.write.author.posts[0].id).toEqual(response.data.write.id);
   });
+
+  it('Mutation: Delete a post', async () => {
+    const post = getPost();
+    const user = getUser();
+    const posts = [post];
+    const users = [user];
+    const { mutate, db } = setupServer(users, posts);
+
+    const DELETE_POST = `mutation { delete(id: "${post.id}")
+    { title, votes, author{name, posts{id}}, id}}`;
+    expect(db.posts.length).toEqual(1);
+    expect(user.posts.length).toEqual(1);
+    const response = await mutate({ mutation: DELETE_POST });
+    expect(response.errors).toBeUndefined();
+    expect(db.posts.length).toEqual(0);
+    expect(response.data.delete.title).toEqual('ABC');
+    expect(response.data.delete.title).toEqual('ABC');
+    expect(response.data.delete.author.name).toEqual('Al');
+    expect(response.data.delete.author.posts.length).toEqual(0);
+  });
+
   it('Mutation: Upvote', async () => {
-    // Setup Server
+    const post = getPost();
+    const user = getUser();
 
-    const Post = getPost();
-    const User = getUser();
+    const posts = [post];
+    const users = [user];
+    const { mutate } = setupServer(users, posts);
 
-    const Posts = [Post];
-    const Users = [User];
-    const query = getQuery(Posts, Users);
-
-    const UPVOTE_POST = `mutation PostInput{
-      upvote (id:"${Post.id}", voter: {name:"${User.name}"}){
-         title, votes, author{name}, id, usersUpvoted
+    const UPVOTE_POST = `mutation {
+      upvote (id:"${post.id}", voter: {name:"${user.name}"}){
+         title, votes, author{name}, id, upvotes
        }
      }`;
-    const response = await query({ query: UPVOTE_POST });
+    const response = await mutate({ mutation: UPVOTE_POST });
     expect(response.errors).toBeUndefined();
     expect(response.data.upvote.title).toEqual('ABC');
     expect(response.data.upvote.votes).toEqual(1);
-    expect(response.data.upvote.usersUpvoted[0]).toEqual('Al');
+    expect(response.data.upvote.upvotes[0]).toEqual('Al');
   });
-  it('Mutation: Double Upvote from same User should not be double upvote', async () => {
-    // Setup Server
 
-    const Post = getPost();
-    const User = getUser();
+  it('Mutation: Double Upvote from same user should not be double upvote', async () => {
+    const post = getPost();
+    const user = getUser();
 
-    const Posts = [Post];
-    const Users = [User];
-    const query = getQuery(Posts, Users);
+    const posts = [post];
+    const users = [user];
+    const { mutate } = setupServer(users, posts);
 
-    const UPVOTE_POST = `mutation PostInput{
-      upvote (id:"${Post.id}", voter: {name:"${User.name}"}){
-         title, votes, author{name}, id, usersUpvoted
+    const UPVOTE_POST = `mutation {
+      upvote (id:"${post.id}", voter: {name:"${user.name}"}){
+         title, votes, author{name}, id, upvotes
        }
      }`;
-    let response = await query({ query: UPVOTE_POST });
+    let response = await mutate({ mutation: UPVOTE_POST });
     expect(response.errors).toBeUndefined();
     expect(response.data.upvote.title).toEqual('ABC');
     expect(response.data.upvote.votes).toEqual(1);
-    expect(response.data.upvote.usersUpvoted[0]).toEqual('Al');
-    response = await query({ query: UPVOTE_POST });
+    expect(response.data.upvote.upvotes[0]).toEqual('Al');
+    response = await mutate({ mutation: UPVOTE_POST });
     expect(response.errors).toBeUndefined();
     expect(response.data.upvote.title).toEqual('ABC');
     expect(response.data.upvote.votes).toEqual(1);
-    expect(response.data.upvote.usersUpvoted[0]).toEqual('Al');
+    expect(response.data.upvote.upvotes[0]).toEqual('Al');
   });
+
   it('Mutation: Downvote', async () => {
-    // Setup Server
+    const post = getPost();
+    const user = getUser();
 
-    const Post = getPost();
-    const User = getUser();
+    const posts = [post];
+    const users = [user];
+    const { mutate } = setupServer(users, posts);
 
-    const Posts = [Post];
-    const Users = [User];
-    const query = getQuery(Posts, Users);
-
-    const DOWNVOTE_POST = `mutation PostInput{
-      downvote (id:"${Post.id}", voter: {name:"${User.name}"}){
-         title, votes, author{name}, id, usersDownvoted
+    const DOWNVOTE_POST = `mutation {
+      downvote (id:"${post.id}", voter: {name:"${user.name}"}){
+         title, votes, author{name}, id, downvotes
        }
      }`;
-    const response = await query({ query: DOWNVOTE_POST });
+    const response = await mutate({ mutation: DOWNVOTE_POST });
     expect(response.errors).toBeUndefined();
     expect(response.data.downvote.title).toEqual('ABC');
     expect(response.data.downvote.votes).toEqual(-1);
-    expect(response.data.downvote.usersDownvoted[0]).toEqual('Al');
+    expect(response.data.downvote.downvotes[0]).toEqual('Al');
   });
-  it('Mutation: First Upvote then Downvote from same User should overwrite the upvote and vote = -1', async () => {
-    // Setup Server
 
-    const Post = getPost();
-    const User = getUser();
+  it('Mutation: First Upvote then Downvote from same user should overwrite the upvote and vote = -1', async () => {
+    const post = getPost();
+    const user = getUser();
 
-    const Posts = [Post];
-    const Users = [User];
-    const query = getQuery(Posts, Users);
+    const posts = [post];
+    const users = [user];
+    const { mutate } = setupServer(users, posts);
 
-    const UPVOTE_POST = `mutation PostInput{
-      upvote (id:"${Post.id}", voter: {name:"${User.name}"}){
-         title, votes, author{name}, id, usersUpvoted
+    const UPVOTE_POST = `mutation {
+      upvote (id:"${post.id}", voter: {name:"${user.name}"}){
+         title, votes, author{name}, id, upvotes
        }
      }`;
-    let response = await query({ query: UPVOTE_POST });
+    let response = await mutate({ mutation: UPVOTE_POST });
     expect(response.errors).toBeUndefined();
     expect(response.data.upvote.title).toEqual('ABC');
     expect(response.data.upvote.votes).toEqual(1);
-    expect(response.data.upvote.usersUpvoted[0]).toEqual('Al');
+    expect(response.data.upvote.upvotes[0]).toEqual('Al');
 
-    const DOWNVOTE_POST = `mutation PostInput{
-      downvote (id:"${Post.id}", voter: {name:"${User.name}"}){
-         title, votes, author{name}, id, usersDownvoted
+    const DOWNVOTE_POST = `mutation {
+      downvote (id:"${post.id}", voter: {name:"${user.name}"}){
+         title, votes, author{name}, id, downvotes
        }
      }`;
-    response = await query({ query: DOWNVOTE_POST });
+    response = await mutate({ mutation: DOWNVOTE_POST });
     expect(response.errors).toBeUndefined();
     expect(response.data.downvote.title).toEqual('ABC');
     expect(response.data.downvote.votes).toEqual(-1);
-    expect(response.data.downvote.usersDownvoted[0]).toEqual('Al');
+    expect(response.data.downvote.downvotes[0]).toEqual('Al');
   });
 });
