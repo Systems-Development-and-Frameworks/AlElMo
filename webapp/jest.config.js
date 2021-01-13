95f58b3104f763f3a4b037b47e0c2f9ccd8939a4
JaielSoft <Elias.Kechter@t-online.de>
2021-01-13 16:17:02 +0100

    init transition to nuxt

--- webapp/jest.config.js
+++ webapp/jest.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-    "moduleFileExtensions": [
-        "js",
-        "json",
-        "vue"
-    ],
-    "transform": {
-        ".*\\.(js)$": "babel-jest",
-        ".*\\.(vue)$": "vue-jest"
-    },
-};
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: [
+    'js',
+    'vue',
+    'json'
+  ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
+  ]
+}
---
+++ webapp/test/Logo.spec.js
@@ -0,0 +1,4 @@
+import { mount } from '@vue/test-utils'
+import Logo from '@/components/Logo.vue'
+
+
